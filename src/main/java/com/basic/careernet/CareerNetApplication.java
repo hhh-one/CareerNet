2f2fd4f045251636ff5a516f22a2ad3e06a5ad38
hhh-one <904lhw@naver.com>
2023-08-07 11:31:44 +0900

    make project

---
+++ src/main/java/com/basic/careernet/CareerNetApplication.java
@@ -0,0 +1,13 @@
+package com.basic.careernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerNetApplication.class, args);
+	}
+
+}
